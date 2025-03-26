e745fd60cfda2ccd59630a5c57e83c710502ac8d
vitor <vitorcostaqz@gmail.com>
2025-03-26 15:26:24 -0300

    Correção de Possíveis Erros

--- Projetos/ProjetoBancario.py
+++ Projetos/ProjetoBancario.py
@@ -7,6 +7,7 @@
 saldo = 0
 request = 0
 sistema = 0
+c = 0
 
 print("""Digite a operação desejada:  
         [1] Extrato
@@ -16,21 +17,35 @@ print("""Digite a operação desejada:
         """)
 
                         
-while (sistema != 4):
+while sistema != 4:
     try:
-        sistema = int(input(""))
+        if c == 0:
+            sistema = int(input(""))
+        else:
+            sistema = int(input("Digite uma nova operação: "))
+
         if sistema == 1:
             print(f"O valor do seu saldo atual é de: {saldo}")
 
         elif sistema == 2:
             request = int(input("Digite a quantidade que deseja sacar: "))
             if saldo > request:
-                saldo = saldo - request
+                    saldo = saldo - request
             else:
-                print("Saldo insuficiente!")
+                print("Saldo insuficiente!") # Caso não tenha saldo
 
         elif sistema == 3:
-            saldo += int(input("Digite a quantidade que deseja depositar: "))
+            valor = -1 # Variável para começar o loop
+            while valor < 0:
+                valor = int(input("Digite a quantidade que deseja depositar: "))
+                if valor > 0:
+                    saldo += valor
+                else:
+                    print("Valor a depositar não pode ser negativo!") # Caso o valor seja negativo
+        
+        elif sistema == 4:
+            break
+        c += 1
 
     except ValueError:
         print("Valor inserido inválido!")
