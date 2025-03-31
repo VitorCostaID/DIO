c9086b5ea87caedcee5955ff23d2414981fd8c93
vitor <vitorcostaqz@gmail.com>
2025-03-31 16:24:26 -0300

    Novas funções de contas

--- Projetos/ProjetoBancario.py
+++ Projetos/ProjetoBancario.py
@@ -5,10 +5,27 @@ request = 0
 sistema = 0
 c = 0
 transacoes = 0
+cpf_busca = 0
+numero_clientes = 0
 hoje = datetime.now().strftime("%d/%m/%Y")
 
 
 extrato = []
+clientes = []
+contas = []
+
+def checar_conta():
+    cpf = input("Digite o CPF: ")  # Get CPF before checking
+
+    while not any(cliente[2] == cpf for cliente in clientes):    
+        print("CPF inserido inválido, por favor crie um cliente para este CPF ou insira um existente.")
+        cpf = input("Digite o CPF novamente: ")
+
+    senha = input("Digite a senha de login: ")
+
+    return senha, cpf
+
+    return senha, cpf
 
 def salvar_saque(saldo, request):
     extrato.append({
@@ -28,12 +45,61 @@ def salvar_deposito(saldo, valor):
         "saldo": saldo
     })
 
+def mostrar_extrato():
+    print("==================== EXTRATO ====================")
+    print(f"- Transações dia {hoje} -")
+    if any(t["data"] == hoje for t in extrato):
+        for t in extrato:
+            print(f"{t['hora']} - Tipo: {t['tipo']} - Valor: {t['valor']} - Saldo: {t['saldo']}")
+    else: 
+        print("Nenhuma transação foi realizada hoje.")
+    print("=================================================")
+
+def criar_cliente():
+    nome = input("Digite o nome do cliente: ")
+    data_de_nascimento = input("Digite a data de nascimento do cliente: ")
+    cpf = -1
+
+    while not any(cliente[2] == cpf for cliente in clientes): 
+        cpf = input("Digite o cpf do cliente: ")
+        if not any(cliente[2] == cpf for cliente in clientes):
+            print("CPF inserido já existe, por favor insira um novo CPF, ou prossiga para criar uma conta.")
+
+    endereco = input("Digite o endereço do cliente: ")
+    clientes.append([nome, data_de_nascimento, cpf, endereco])
+
+def criar_conta(numero_clientes):
+    numero_clientes += 1
+    digito_conta = "0001-" + numero_clientes
+    saldo = 0
+
+    senha, cpf = checar_conta()
+
+    contas.append([cpf, digito_conta, saldo, senha])
+
+def logar_conta():
+    senha, cpf = checar_conta()
+
+    for conta in contas:  
+        if conta[0] == cpf:  # Checar o cpf  
+            if conta[3] == senha:  # Checar a senha 
+                print("Login bem-sucedido!")  
+            else:  
+                print("Senha incorreta.")  
+                break  
+        else:  
+            print("CPF não encontrado.") 
+
+
+
 
 print("""Digite a operação desejada:  
         [1] Extrato
         [2] Saque
         [3] Depósito
         [4] Sair
+        [cc] Criar Conta
+        [ag] Cadastrar Agência
         """)
 
 
@@ -41,17 +107,12 @@ print("""Digite a operação desejada:
 while sistema != 4: # [Sistema bancário]
     try:
         if c == 0: # [Mensagem]
-            sistema = int(input(""))
+            sistema = input("")
         else:
-            sistema = int(input("Digite uma nova operação: "))
+            sistema = input("Digite uma nova operação: ")
 
         if sistema == 1: # [Extrato]
-            print(f"- Transações dia {hoje} -")
-            if any(t["data"] == hoje for t in extrato):
-                for t in extrato:
-                    print(f"{t['hora']} - Tipo: {t['tipo']} - Valor: {t['valor']} - Saldo: {t['saldo']}")
-            else: 
-                print("Nenhuma transação foi realizada hoje.")
+            mostrar_extrato()
 
         elif sistema == 2: # [Saque]
             if sum(1 for t in extrato if t["data"] == hoje) < 10:
@@ -81,6 +142,16 @@ while sistema != 4: # [Sistema bancário]
             
         elif sistema == 4: # [Sair]
             break
+
+        elif sistema == "cc": # [Criar Conta]
+            criar_conta()
+
+        elif sistema == "cl": # [Cadastrar Cliente]
+            criar_cliente(numero_clientes)
+
+        elif sistema == "e": # [Entrar na conta]
+            logar_conta()
+
         c += 1
 
     except ValueError:
